26c5993649eabef7a63030fadb53ab50f6f51b00
siwangqishiq <525647740@qq.com>
2013-02-06 16:35:04 +0800

    首次提交代码

---
+++ Pong/src/com/xinlan/pong/MainActivity.java
@@ -0,0 +1,20 @@
+package com.xinlan.pong;
+
+import com.example.pong.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+/**
+ * 第一次提交
+ * @author Panyi
+ *
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
